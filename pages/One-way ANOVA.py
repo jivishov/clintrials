7082f43d5b8b83766c10c9a04dcdc09222f97f21
jivishov <97884055+jivishov@users.noreply.github.com>
2023-09-12 23:54:12 -0500

    Add files via upload

---
+++ pages/One-way ANOVA.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+from scipy.stats import f_oneway
+
+# Streamlit layout and logic
+st.title("One-way ANOVA Calculator")
+
+# # Sidebar for future menu options
+# st.sidebar.title("Menu")
+# st.sidebar.text("Future options will go here.")
+
+# Upload CSV file
+uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
+
+if uploaded_file is not None:
+    # Read and display data
+    df = pd.read_csv(uploaded_file)
+    st.write("Preview of uploaded data:")
+    st.write(df.head())
+
+    # Select columns
+    selected_columns = st.multiselect("Select columns for One-way ANOVA", df.columns.tolist())
+
+    if len(selected_columns) >= 2:
+        # Perform One-way ANOVA
+        selected_data = [df[col] for col in selected_columns]
+        f_stat, p_value = f_oneway(*selected_data)
+
+        # Display results
+        st.write(f"F-statistic: **{f_stat:.4f}**")
+        st.write(f"p-value: **{p_value:.4f}**")
+
+        # Interpret p-value
+        alpha = 0.05
+        if p_value < alpha:
+            st.write("The p-value is less than 0.05, suggesting that there is a statistically significant difference between the groups.")
+        else:
+            st.write("The p-value is greater than 0.05, indicating that there's not a statistically significant difference between the groups.")
---
+++ pages/Wilcoxon_Signed-Rank_Test.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+from scipy.stats import wilcoxon
+
+# Streamlit layout and logic
+st.title("Wilcoxon Signed-Rank Test Calculator")
+
+# # Sidebar for future menu options
+# st.sidebar.title("Menu")
+# st.sidebar.text("Future options will go here.")
+
+# Upload CSV file
+uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
+
+if uploaded_file is not None:
+    # Read and display data
+    df = pd.read_csv(uploaded_file)
+    st.write("Preview of uploaded data:")
+    st.write(df.head())
+
+    # Select columns
+    selected_columns = st.multiselect("Select two columns for Wilcoxon Signed-Rank Test", df.columns.tolist())
+
+    if len(selected_columns) == 2:
+        # Perform Wilcoxon Signed-Rank Test
+        col1_data = df[selected_columns[0]]
+        col2_data = df[selected_columns[1]]
+
+        w_stat, p_value = wilcoxon(col1_data, col2_data)
+
+        # Display results
+        st.write(f"W-statistic: **{w_stat:.4f}**")
+        st.write(f"p-value: **{p_value:.4f}**")
+
+        # Interpret p-value
+        alpha = 0.05
+        if p_value < alpha:
+            st.write("The p-value is less than 0.05, suggesting that there is a statistically significant difference between the groups.")
+        else:
+            st.write("The p-value is greater than 0.05, indicating that there's not a statistically significant difference between the groups.")
